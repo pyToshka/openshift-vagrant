7570dfe05fe208362c95dc6846ab32240f5bdfd1
Anton Ermakov <a.ermakov@Worker.local>
2019-01-25 16:41:42 +0300

    work

--- ansible/roles/openshift/templates/backupimage.sh
+++ ansible/roles/openshift/templates/backupimage.sh
@@ -7,21 +7,17 @@ podman images | awk 'NR>1 {print $0}' | while read -r line; do
     img_name_old=$(echo "$line"  | awk '{print $1}')
     img_version=$(echo "$line"  | awk '{print $2}')
 # Searchh registry
-    img_name_k8s=$(echo "$line"| awk '{print $1}' | sed 's#k8s.gcr.io##g' )
-    img_name_docker=$(echo "$line"| awk '{print $1}' | sed 's#docker.io##g' )
-    img_name_quay=$(echo "$line"| awk '{print $1}' | sed 's#quay.io##g' )
+    img_name_docker=$(echo "$line"| awk '{print $1}' | sed 's#docker.io#{repository}#g' )
+    img_name_quay=$(echo "$line"| awk '{print $1}' | sed 's#quay.io#{repository}#g' )
+    img_name_rh=$(echo "$line"| awk '{print $1}' | sed 's#registry.centos.org#{repository}#g' )
 
 # Patterns for sed
     k8s="k8s.gcr.io"
     docker="docker.io"
     quay="quay.io"
+    rh="registry.centos.org"
 
 # Tag commands
-    if  [[ "$line" == *"$k8s"* ]]; 
-    then
-        podman tag "$img_name_old":"$img_version" "$img_name_k8s":"$img_version"
-    fi
-
     if [[ "$line" == *"$docker"* ]];
     then
         podman tag "$img_name_old":"$img_version" "$img_name_docker":"$img_version"
@@ -31,6 +27,11 @@ podman images | awk 'NR>1 {print $0}' | while read -r line; do
     then
         podman tag "$img_name_old":"$img_version" "$img_name_quay":"$img_version"
     fi
+
+    if [[ "$line" == *"$rh"* ]];
+    then
+        podman tag "$img_name_old":"$img_version" "$img_name_rh":"$img_version"
+    fi
 done
 
 # Push part
@@ -39,7 +40,7 @@ podman images | grep harbor | while read -r line; do
 # Main environments
     img_name=$(echo "$line"  | awk '{print $1}')
     img_version=$(echo "$line"  | awk '{print $2}')
-# Push commands with creds
-    # podman login -u {username} -p {password} {repository}
-    podman push --creds {username}:{password} "$img_name":"$img_version"
+# Push commands with creds.
+    podman login -u {user} -p {password} {repository}
+    podman push  "$img_name":"$img_version"
 done
